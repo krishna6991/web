2c4ca9c4b332cd41b5ad1000ab8ec94d6e495114
krishna <neversaykrishna@gmail.com>
2017-11-19 00:32:59 +0530

    Python Learning websites

---
+++ learn_python/Courses/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Course, Step
+
+def course_list(request):
+    courses=Course.objects.all()
+    #output=', '.join([str(course) for course in courses])
+    #return HttpResponse(output)
+    return render(request, 'Courses/course_list.html', {'Courses':courses}  )
+
+def course_detail(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+    return render(request, 'Courses/course_detail.html', {'course': course})
+
+def step_detail(request, course_pk, step_pk):
+    step = get_object_or_404(Step, course_id=course_pk, pk=step_pk)
+    return render(request, 'Courses/step_detail.html', {'step':step})
